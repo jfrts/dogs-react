a060e36c6322b8ae003b7e78a0f3b863016e9f9f
Júlio Freitas <jcsr.frts@gmail.com>
2022-11-06 14:23:54 -0300

    feat: login function

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 
 import { LoginForm } from "./subcomponents/LoginForm";
+import { LoginCreate } from "./subcomponents/LoginCreate";
+import { LoginPasswordLost } from "./subcomponents/LoginPasswordLost";
+import { LoginPasswordReset } from "./subcomponents/LoginPasswordReset";
 
 export function Login() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={"<LoginCreate />"} />
-        <Route path="/recuperar-senha" element={"<LoginPasswordLost />"} />
-        <Route path="/resetar-senha" element={"<LoginPasswordReset />"} />
+        <Route path="/criar" element={<LoginCreate />} />
+        <Route path="/recuperar-senha" element={<LoginPasswordLost />} />
+        <Route path="/resetar-senha" element={<LoginPasswordReset />} />
       </Routes>
     </>
   );
