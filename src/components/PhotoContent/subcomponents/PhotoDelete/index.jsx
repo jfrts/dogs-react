783d3b23c19fc0ac758e207aa90ab5aa0baeddc4
Júlio Freitas <jcsr.frts@gmail.com>
2022-12-11 21:14:39 -0300

    fix: reload after delete post

--- src/components/PhotoContent/subcomponents/PhotoDelete/index.jsx
+++ src/components/PhotoContent/subcomponents/PhotoDelete/index.jsx
@@ -2,9 +2,11 @@ import "./styles.css";
 import useFetch from "@/hooks/useFetch";
 import { DELETE_PHOTO } from "@/api";
 import { RenderIf } from "@/components/render-if";
+import { useNavigate } from "react-router-dom";
 
 export function PhotoDelete({ id }) {
   const { request, loading } = useFetch();
+  const navigate = useNavigate();
 
   async function deletePhoto() {
     const confirm = window.confirm("Tem certeza que deseja deletar essa postagem?");
@@ -15,7 +17,7 @@ export function PhotoDelete({ id }) {
     const { response } = await request(url, options);
 
     if (response.ok) {
-      window.location.reload();
+      navigate("/");
     }
   }
 
