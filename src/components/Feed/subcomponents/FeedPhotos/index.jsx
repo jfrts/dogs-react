c1d81699e0760679e0fb31b31364b5f44b86f858
Júlio Freitas <jcsr.frts@gmail.com>
2022-12-13 08:46:51 -0300

    fix: feed

--- src/components/Feed/subcomponents/FeedPhotos/index.jsx
+++ src/components/Feed/subcomponents/FeedPhotos/index.jsx
@@ -32,25 +32,11 @@ export function FeedPhotos({ user, page, setModalPhoto, setInfinite }) {
 
   if (data) {
     return (
-<<<<<<< HEAD
       <RenderIf condition={data.length > 0}>
         <ul className="animeLeft feed">
           {data.map((photo) => <FeedPhotosItem photo={photo} setModalPhoto={setModalPhoto} key={photo.id} />)}
         </ul>
       </RenderIf>
-=======
-      <>
-        <RenderIf condition={data.length > 0}>
-          <ul className="animeLeft feed">
-            {data.map((photo) => <FeedPhotosItem photo={photo} setModalPhoto={setModalPhoto} key={photo.id} />)}
-          </ul>
-        </RenderIf>
-
-        <RenderIf condition={data.length === 0}>
-          <h2 className="empty-message">Nenhuma postagem para ser mostrada...</h2>
-        </RenderIf>
-      </>
->>>>>>> fc0a21c5ea039a55a5dafa9e80f2337d1aa943b2
     )
   }
 }
