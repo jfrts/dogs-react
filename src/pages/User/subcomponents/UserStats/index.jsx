6e092be3b5cf34403ef971d641254ae884f86231
Júlio Freitas <jcsr.frts@gmail.com>
2022-12-14 10:54:50 -0300

    feat: lazy and suspense

--- src/pages/User/subcomponents/UserStats/index.jsx
+++ src/pages/User/subcomponents/UserStats/index.jsx
@@ -1,10 +1,10 @@
 import { Head } from "@/components/Head";
 import useFetch from "@/hooks/useFetch";
 import { GET_USER_STATS } from "@/api";
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { Loading } from "@/components/helpers/Loading";
 import { Error } from "@/components/helpers/Error";
-import { UserStatsCharts } from "./subcomponents/UserStatsCharts";
+const UserStatsCharts = lazy(() => import("./subcomponents/UserStatsCharts"));
 
 export function UserStats() {
   const { data, error, loading, request } = useFetch();
@@ -27,10 +27,10 @@ export function UserStats() {
 
   if (data) {
     return (
-      <>
+      <Suspense fallback={<></>}>
         <Head title="Estatísticas" />
         <UserStatsCharts data={data} />
-      </>
+      </Suspense>
     )
   } else return null;
 }
