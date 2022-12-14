5ee29d27b640bc056afbe34d13b0f34bb97dfda3
Júlio Freitas <jcsr.frts@gmail.com>
2022-12-14 10:30:55 -0300

    feat: charts

--- src/pages/User/subcomponents/UserStats/index.jsx
+++ src/pages/User/subcomponents/UserStats/index.jsx
@@ -1,7 +1,36 @@
 import { Head } from "@/components/Head";
+import useFetch from "@/hooks/useFetch";
+import { GET_USER_STATS } from "@/api";
+import { useEffect } from "react";
+import { Loading } from "@/components/helpers/Loading";
+import { Error } from "@/components/helpers/Error";
+import { UserStatsCharts } from "./subcomponents/UserStatsCharts";
 
 export function UserStats() {
-  return (
-    <Head title="Estatísticas" />
-  )
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(function () {
+    async function getUserStats() {
+      const { url, options } = GET_USER_STATS();
+      await request(url, options);
+    }
+    getUserStats();
+  }, [request]);
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <Error errorMessage={error} />
+  }
+
+  if (data) {
+    return (
+      <>
+        <Head title="Estatísticas" />
+        <UserStatsCharts data={data} />
+      </>
+    )
+  } else return null;
 }
---
+++ src/pages/User/subcomponents/UserStats/subcomponents/UserStatsCharts/index.jsx
@@ -0,0 +1,59 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+import { VictoryPie, VictoryChart, VictoryBar, VictoryTheme } from "victory";
+
+export function UserStatsCharts({ data }) {
+  const [chart, setChart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(function () {
+    if (data && data.length > 0) {
+      const chartData = data.map(item => ({ x: item.title, y: Number(item.acessos) }));
+      setChart(chartData);
+
+      const sumTotal = data
+        .map(({ acessos }) => Number(acessos))
+        .reduce((acc, currentValue) => acc + currentValue);
+
+      setTotal(sumTotal);
+    }
+  }, [data]);
+
+  if (data && data.length > 0) {
+    return (
+      <section className="animeLeft charts-section">
+        <div className="total chart-item">
+          Acessos: {total}
+        </div>
+        <div className="chart-item">
+          <VictoryPie
+            data={chart}
+            innerRadius={60}
+            padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+            theme={VictoryTheme.material}
+            style={{
+              data: {
+                fillOpacity: 0.9,
+                stroke: "#0d1117",
+                strokeWidth: 2
+              },
+              labels: {
+                fontSize: 14,
+                fill: "#f0f6fc"
+              }
+            }}
+          />
+        </div>
+        <div className="chart-item">
+          <VictoryChart
+            theme={VictoryTheme.material}
+            domainPadding={48}
+            dependentAxis
+          >
+            <VictoryBar data={chart} alignment="start"></VictoryBar>
+          </VictoryChart>
+        </div>
+      </section>
+    );
+  } else return null;
+}
